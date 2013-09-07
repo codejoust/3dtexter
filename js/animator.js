5aecdb884716ff6a6b5e2c39338b0f3c02a68b37
Iain Nash <info@codejoust.com>
2013-09-07 19:32:34 -0400

    cleaning up rendering v0.0.0.0.04

--- js/animator.js
+++ js/animator.js
@@ -1,6 +1,6 @@
 window.exports = {};
-window.exports.load = function(){};
 
+window.exports.load = function(){
 	var opts = {
 		container: document.getElementById('text_container'),
 		camera: null,
@@ -13,18 +13,23 @@ window.exports.load = function(){};
 		targetRotation: 0
 	};
 
-	function setup(){
+	this.setup = function(){
 		opts.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
 		opts.camera.position.set( 0, 150, 500 );
 		opts.scene = new THREE.Scene();
 
 	}
 
-	function drawTextInternal(text){
+	this.drawTextInternal = function(text){
 		var text3d = new THREE.TextGeometry( text, {
-			size: 80,
-			height: 20,
-			curveSegments: 2,
+			size: 120,
+			height: 60,
+			hover: 20,
+			curveSegments: 4,
+			bevelThickness: 0.02,
+			belvelSize: 0.02,
+			bevelSegments: 8,
+			bevelEnabled: false,
 			font: "blackout",
 			weight: 'bold'
 		});
@@ -32,21 +37,22 @@ window.exports.load = function(){};
 		text3d.computeBoundingBox();
 		var centerOffset = -0.5 * ( text3d.boundingBox.max.x - text3d.boundingBox.min.x );
 
-		var textMaterial = new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, overdraw: true } );
+		var textMaterial =  new THREE.MeshPhongMaterial( { color: Math.random()*0xffffff, shading: THREE.SmoothShading } );
+
 
 		text = new THREE.Mesh( text3d, textMaterial );
 		opts.text.canvas = text;
 
 		text.position.x = centerOffset;
 		text.position.y = 100;
-		text.position.z = 0;
+		text.position.z = -40;
 
-		text.rotation.x = 0;
+		text.rotation.x = -0.23;
 		text.rotation.y = Math.PI * 2;
 		return text;
 	}
 
-	function setupCanvas(){
+	this.setupCanvas = function(){
 
 		opts.group = new THREE.Object3D();
 		opts.group.add( opts.text.canvas );
@@ -54,14 +60,14 @@ window.exports.load = function(){};
 		opts.scene.add( opts.group );
 
 		opts.renderer = new THREE.CanvasRenderer();
-		opts.renderer.setSize( 500, 300 );
+		opts.renderer.setSize( 800, 300 );
 
 		opts.container.appendChild( opts.renderer.domElement );
 
 	}
 
 
-	function bindEvents(){
+	this.bindEvents = function(){
 
 		document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 		document.addEventListener( 'touchstart', onDocumentTouchStart, false );
@@ -69,22 +75,23 @@ window.exports.load = function(){};
 		window.addEventListener( 'resize', onWindowResize, false );
 
 	}
-
-	function render(){
-		
+	
+	this.render = function(){
 		opts.group.rotation.y += ( opts.targetRotation - opts.group.rotation.y ) * 0.05;
 		opts.renderer.render( opts.scene, opts.camera );
 	}
 
-	function animate(){
+	this.animate = function(){
 		requestAnimationFrame( animate );
 		render();
 	}
    
     // actually init
-   	setup();
-   	drawTextInternal('hello world');
-   	setupCanvas();
-   	render(); 
+   	this.setup();
+   	this.drawTextInternal('hello world');
+   	this.setupCanvas();
+   	this.render(); 
+
+}
 
 
